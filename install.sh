dbd7e48b9c263fc4cb7473598eea88c13d0b1c1a
calvyty <85029625+calvyty@users.noreply.github.com>
2022-12-27 12:16:05 +0100

    Reformat post install commands

--- install.sh
+++ install.sh
@@ -165,32 +165,56 @@ brew doctor
 # macOS Settings
 echo "Setting sensible macOS defaults..."
 
-# General: Increase window resize speed for Cocoa applications
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+# Activity Monitor: Show all processes
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-# General: Always show scroll bars
-defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Activity Monitor: Show the main window when launching
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-# General: Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+# Dock: Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false && \
+killall Dock
 
-# General: Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+# Dock: Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
-# General: Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+# Dock: Make icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
 
-# General: Disable auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Dock: Only show active applications in the Dock
+#defaults write com.apple.dock static-only -bool true
 
-# General: Enable Full Keyboard Access (Tab in modal dialogs)
-#defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# Dock: Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
 
-# General: Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+# Dock: Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Dock: Reset Launchapd Layout
+defaults write com.apple.dock ResetLaunchPad -bool true && \
+killall Dock
+
+# Dock: Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Dock: Show indicators for open applications
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Dock: Use the touchpad or mouse scroll wheel to interact with Dock items
+defaults write com.apple.dock scroll-to-open -bool true && \
+killall Dock
+
+# Finder: Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Finder: Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Finder: Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Finder: Show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
@@ -198,29 +222,12 @@ defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Finder: Show hidden files by default
-defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Finder: Show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Finder: Show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
-
-# Finder: Show path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Finder: Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
 # Finder: Keep folders on top when sorting by name
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Finder: When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-# Finder: Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Finder: Set Default Finder Location to Home Folder
+defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
 
 # Finder: Show the ~/Library folder
 chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
@@ -228,57 +235,41 @@ chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
 # Finder: Show the /Volumes folder
 sudo chflags nohidden /Volumes
 
-# Finder: Set Default Finder Location to Home Folder
-defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+# Finder: Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
-# Dock: Remove the auto-hiding Dock delay
-defaults write com.apple.dock autohide-delay -float 0
+# Finder: Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
 
-# Dock: Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
+# Finder: When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Dock: Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 36
+# General: Always show scroll bars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
-# Dock: Only show active applications in the Dock
-#defaults write com.apple.dock static-only -bool true
+# General: Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Dock: Make icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
+# General: Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Dock: Don’t show recent applications in Dock
-defaults write com.apple.dock show-recents -bool false
+# General: Enable Full Keyboard Access (Tab in modal dialogs)
+#defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Dock: Show indicators for open applications
-defaults write com.apple.dock show-process-indicators -bool true
+# General: Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# Dock: Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false && \
-killall Dock
+# General: Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-# Dock: Use the touchpad or mouse scroll wheel to interact with Dock items
-defaults write com.apple.dock scroll-to-open -bool true && \
-killall Dock
+# General: Increase window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
-# Dock: Reset Launchapd Layout
-defaults write com.apple.dock ResetLaunchPad -bool true && \
-killall Dock
-
-# Safari: Prevent Safari from opening ‘safe’ files automatically after downloading
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-
-# Safari: Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-# Safari: Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Safari: Use Backspace to Go Back a Page
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+# General: Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Mail: Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
 #defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
@@ -286,17 +277,26 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 # Mail: Show Attachments as Icons
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 
+# Safari: Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Safari: Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Safari: Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+# Safari: Use Backspace to Go Back a Page
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
 # TextEdit: Create an Untitled Document at Launch
 defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
 
 # TextEdit: Use plain text as default
 defaults write com.apple.TextEdit RichText -int 0
-
-# Activity Monitor: Show the main window when launching
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
-# Activity Monitor: Show all processes
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
 # Restart Dock
 echo "Restarting Dock..."
